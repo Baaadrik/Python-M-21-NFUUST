bc551cc222db974c9676490677f4ce43f01a2eb2
Badrik <denbadrik@mail.ru>
2023-12-06 22:11:06 +0300

    https://github.com/Baaadrik/Python-M-21-NFUUST.git

---
+++ 01/01-05.py
@@ -0,0 +1,10 @@
+word_1 = str(input())
+word_2 = str(input())
+word_3 = str(input())
+
+if (word_1 == 'раз' or word_1 == 'один') and word_2 == 'два' and word_3 == 'три':
+	print('ГОРИ')
+elif word_1 == '1' and word_2 == '2' and word_3 == '3':
+	print('ГОРИ')
+else:
+	print('НЕ ГОРИ')
