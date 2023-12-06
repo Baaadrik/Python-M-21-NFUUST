af90bb52c7cf1c744d991fac999ab10f0c1996de
Badrik <denbadrik@mail.ru>
2023-12-06 21:53:23 +0300

    https://github.com/Baaadrik/Python-M-21-NFUUST.git

---
+++ 00/00-08.py
@@ -0,0 +1,8 @@
+first_name = input()
+second_name = input()
+like_animal = input()
+zodiac_sign = input()
+
+print('Индивидуальный гороскоп для пользователя '+first_name+' '+second_name)
+print('Кем вы были в прошлой жизни: ' + like_animal)
+print('Ваш знак зодиака - '+zodiac_sign+' , поэтому вы - тонко чувствующая натура.')
