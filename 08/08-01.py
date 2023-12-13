321cfec6c0648234d8b87dbafd954f6bf89cca8a
Badrik <denbadrik@mail.ru>
2023-12-13 20:33:32 +0300

    https://github.com/Baaadrik/Python-M-21-NFUUST.git

---
+++ 08/08-01.py
@@ -0,0 +1,13 @@
+a = input()
+words = []
+
+while a != "стоп":
+    words.append(a)
+    a = input()
+
+ma, mi = max(words, key=len), min(words, key=len)
+
+if set(ma).issuperset(set(mi)):
+    print("ДА")
+else:
+    print("НЕТ")
