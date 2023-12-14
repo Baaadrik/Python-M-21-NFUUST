037434569a4f9184523f45d1c747218782fe4cb7
Badrik <denbadrik@mail.ru>
2023-12-14 15:13:29 +0300

    https://github.com/Baaadrik/Python-M-21-NFUUST.git

---
+++ 09/09-04.py
@@ -0,0 +1,12 @@
+#~/college/python-nfbgu/09/
+num = int(input())
+if num > 0:
+    check = list()
+    for i in range(n):
+        check.append(input())
+    b = int(input())
+
+    if b > 0:
+        for i in range(j):
+            number = int(input())
+            print(check[number - 1])
